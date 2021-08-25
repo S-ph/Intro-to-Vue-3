49f69ca2e9c80152601f7f4b67188bc4fce5d5d8
S-ph <santiph@edu.unirio.br>
2021-08-24 22:36:50 -0300

    L3 final
    L3

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: "./assets/images/socks_blue.jpg",
+            description: "Good Looking Socks",
+            url: 'https://www.vuemastery.com/'
         }
     }
 })
